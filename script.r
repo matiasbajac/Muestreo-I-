d7f5849582a9844dade8e2696b81c18e63120102
Andrés Vidal <andres@wyeworks.com>
2023-04-22 14:33:18 -0300

    Estimación de la distribución empirica de t_prw en NBI
    Signed-off-by: Andrés Vidal <andres@wyeworks.com>

--- script.r
+++ script.r
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(haven)
+library(survey)
+library(ggplot2)
 
 # Limpiar el ambiente
 
@@ -15,9 +17,54 @@ is_nbi_missing <- function(x) x %in% c(8, 9)
 is_nbi_var <- function() starts_with("NBI_")
 mutate_nbi <- function(x) zap_labels(x) %>% if_else(is_nbi_missing(.), 0, .)
 select_nbi <- function(x) select(x, is_nbi_var(), -NBI_CANTIDAD)
+has_xo <- function(x) if_else(x$HOGCE09 == 0, 1, 0)
+has_nbi <- function(x) if_else(x$NBI_TOTAL >= 4, 1, 0)
 
 # Preprocesamiento
-
 data <- rio_branco %>%
   mutate(across(is_nbi_var(), ~ mutate_nbi(.))) %>%
-  mutate(NBI_TOTAL = rowSums(select_nbi(.)))
+  mutate(NBI_TOTAL = rowSums(select_nbi(.)), XO = has_xo(.)) %>%
+  mutate(NBI = has_nbi(.)) %>%
+  filter(!duplicated(ID_VIVIENDA)) %>%
+  select(NBI, XO)
+
+
+# Estimación de NBI mediante el diseño SIR con HT
+set.seed(0)
+
+n_simulations <- 1000
+
+simulate_t_pwr_sir <- function(sample_size) {
+  replicate(n_simulations, {
+    s <- sample(data$NBI, sample_size, replace = TRUE)
+    p <- replicate(sample_size, 1 - (1 - 1 / nrow(data))^sample_size)
+    sum(s / p)
+  })
+}
+
+
+sample_sizes <- c(150, 600, 1000)
+
+t_pwr_sir <- data.frame(
+  lapply(sample_sizes, simulate_t_pwr_sir)
+)
+colnames(t_pwr_sir) <- paste0("n_", sample_sizes)
+
+
+empirical_distribution <- function(n) {
+  var <- paste("n", n, sep = "_")
+  binwidth <- 3.5 * sd(t_pwr_sir[[var]]) / (n^(1 / 3))
+  ggplot(t_pwr_sir, aes(x = !!sym(var), y = ..density..)) +
+    geom_histogram(binwidth = binwidth, color = "black", fill = "white") +
+    geom_density(alpha = 0.2, fill = "blue") +
+    labs(x = "Data", y = "Density") +
+    theme_classic()
+
+  ggsave(
+    paste("t_pwr_sir_", var, ".png", sep = ""),
+    width = 10, height = 10, dpi = 300
+  )
+}
+
+
+for (n in sample_sizes) empirical_distribution(n)
